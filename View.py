ea97cd77b6f7e5391d5dedd655e0b2c2ac83e926
Viktoriia Brykalova <viktorytori1810@gmail.com>
2017-05-23 23:51:29 +0300

    add View module

---
+++ View.py
@@ -0,0 +1,51 @@
+"""
+It is unit with functions that print different messages
+"""
+
+
+def print_menu():
+    print("1:. Show all phonebook")
+    print("2:. Get record from phonebook by full name")
+    print("3:. Add record to phonebook")
+    print("4:. Delete record from phonebook")
+    print("5:. Exit")
+
+
+def print_input_first_name():
+    print("Please, input first name: ")
+
+
+def print_input_last_name():
+    print("Please, input last name: ")
+
+
+def print_input_phone_number():
+    print("Please, input phone number: ")
+
+
+def print_confirm_question():
+    print("Are you sure?  y/n")
+
+
+def print_delete_message():
+    print("Record removed!")
+
+
+def print_add_message():
+    print("Record added!")
+
+
+def print_if_record_exist():
+    print("This record already exist!")
+
+
+def print_if_record_absent():
+    print("No such record!")
+
+
+def print_program_completed():
+    print("____Program completed____")
+
+
+def print_if_database_empty():
+    print("Database is empty!")
